4b21f2d106e7e71ade41734a943b51b22aec362e
leijohansson <linnealeijohansson@outlook.com>
2024-06-19 21:41:49 +0100

    Time Series for report

--- Oceans_APE.py
+++ Oceans_APE.py
@@ -15,6 +15,11 @@ import numpy as np
 import pickle 
 import matplotlib.dates as mdates
 from FuncsAPE import find_depthfracs, datapath
+import seaborn as sns
+palette = sns.color_palette("tab10", 10)[::2]
+sns.set_theme(context='paper', style='white', palette=palette,
+              rc={'xtick.bottom': True,'ytick.left': True,})
+#%%
 # from polygons_EN4 import oceans
 #set max depth to take
 max_depth = np.inf
@@ -72,7 +77,7 @@ rolling_n = 12
 df = pd.DataFrame()
 for OB in ocean_filters.keys():
     df[OB] = TS_oceans[OB]
-rolling = df.rolling(rolling_n)
+rolling = df.rolling(rolling_n, center = True)
 
 #%%
 #Plots
@@ -80,11 +85,11 @@ fig, axs = plt.subplots(4, 3, figsize=(12, 15))
 f_i = 0
 for OB in TS_oceans.keys():
     #plotting data
-    axs[f_i//3, f_i%3].plot(x_time, TS_oceans[OB], alpha = 0.6)
+    axs[f_i//3, f_i%3].plot(x_time, TS_oceans[OB], alpha = 0.6, label = 'Data')
     #plotting rolling mean
-    axs[f_i//3, f_i%3].plot(x_time, rolling.mean()[OB], color = 'black')
+    axs[f_i//3, f_i%3].plot(x_time, rolling.mean()[OB], color = 'black', label = f'{rolling_n} month rolling mean')
     axs[f_i//3, f_i%3].set_title(OB)
-    axs[f_i//3, f_i%3].set_ylabel('Volume Integrated APE, $Jm^{-3}$')
+    axs[f_i//3, f_i%3].set_ylabel('Volume Integrated APE, $J$')
     f_i += 1
     
 
@@ -92,5 +97,60 @@ axs[3, 0].set_xlabel('Time')
 axs[3, 1].set_xlabel('Time')
 fig.suptitle(f'Volume Integrated APE, depths < {max_depth}')
 fig.tight_layout()
+
+axs[-1, -1].axis('off')
+axs[-1, -1].plot([], alpha = 0.6, label = 'Data')
+axs[-1, -1].plot([], color = 'black', label = f'{rolling_n} month rolling mean')
+axs[-1, -1].legend(fontsize = 12, loc = 'upper left')
+
 # fig.savefig('EN4 Plots/Ocean_APE_TS.pdf', bbox_inches = 'tight')
+
+#%%
+def plot_one(OB, ax):
+    ax.set_title(OB)
+    ax.plot(x_time, TS_oceans[OB], alpha = 0.6, label = 'Data')
+    #plotting rolling mean
+    ax.plot(x_time, rolling.mean()[OB], color = 'black', label = f'{rolling_n} month rolling mean')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Volume Integrated APE, $J$')
+
+    
+#%%
+#pacific
+fig, axs = plt.subplots(2,  1, sharex = True)
+plot_one('North Pacific Ocean', axs[0])
+plot_one('South Pacific Ocean', axs[1])
+fig.tight_layout()
+axs[0].legend(loc = 'upper right')
+plt.savefig('EN4 Plots/Pacific_TS.pdf')
+
+#%%
+fig, axs = plt.subplots(2,  1, sharex = True)
+plot_one('North Atlantic Ocean', axs[0])
+plot_one('South Atlantic Ocean', axs[1])
+fig.tight_layout()
+axs[0].legend(loc = 'upper right')
+plt.savefig('EN4 Plots/Atlantic_TS.pdf')
+
+#%%
+fig, axs = plt.subplots(2,  1, sharex = True)
+plot_one('Arctic Ocean', axs[0])
+plot_one('Southern Ocean', axs[1])
+fig.tight_layout()
+axs[0].legend(loc = 'lower left')
+plt.savefig('EN4 Plots/Poles_TS.pdf')
+
+print('Peak at:', x_time[np.where(TS_oceans['Arctic Ocean'] == TS_oceans['Arctic Ocean'].max())][0])
+x_warm = pd.date_range('2016-01-30', periods=1, freq='m')
+
+print('Arctic warming ', x_warm)
+axs[0].vlines(x_warm, 2.7e20, 3e20)
+#coincides with arctic warming. 
+#%%
+fig, ax = plt.subplots(1,  1, sharex = True, figsize = (6.4, 3))
+plot_one('Indian Ocean', ax)
+fig.tight_layout()
+ax.legend(loc = 'upper left')
+plt.savefig('EN4 Plots/Indian_TS.pdf')
+    
         
